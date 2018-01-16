c31f48f5b36161ce92082ff36440f33ad7d9c579
lonnen <chris.lonnen@gmail.com>
2018-01-15 21:17:44 -0800

    calcify check_addon config value
    the value is true in every environment and all tests. this refactors the rule to reflect that and remove the config value

--- socorro/unittest/processor/test_mozilla_transform_rules.py
+++ socorro/unittest/processor/test_mozilla_transform_rules.py
@@ -389,7 +389,6 @@ class TestAddonsRule(TestCase):
     def get_basic_config(self):
         config = CDotDict()
         config.logger = Mock()
-        config.collect_addon = True
         config.chatty = False
 
         return config
@@ -984,7 +983,6 @@ class TestProductRewrite(TestCase):
     def get_basic_config(self):
         config = CDotDict()
         config.logger = Mock()
-        config.collect_addon = True
         config.chatty = False
 
         return config
